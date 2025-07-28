98a68c64fc9699bef28b997b43ad21d7cd9e05f1
matoo11 <aliimahdi299911@gmail.com>
2025-07-28 21:51:59 +0300

    radial

--- public/assets/js/query.js
+++ public/assets/js/query.js
@@ -119,3 +119,33 @@ export async function fetchSkills() {
     return res?.data?.transaction || [];
   }
   
+
+  export async function fetchSkills2() {
+    const query = `
+       query {
+        transaction(
+            where: {
+                _and: [
+                    {type: { _iregex: "(^|[^[:alnum:]_])[[:alnum:]_]*skill_[[:alnum:]_]*($|[^[:alnum:]_])" }},
+                    {type: {_like: "%skill%"}},
+                    {object: {type: {_eq: "project"}}},
+                    {type: {_in: [
+                         "skill_git", "skill_go", "skill_js", 
+                        "skill_html", "skill_css", "skill_unix", "skill_docker", 
+                        "skill_sql"
+                    ]}}
+                ]
+            }
+            order_by: [{type: asc}, {createdAt: desc}]
+            distinct_on: type
+        ) {
+            amount
+            type
+        }
+    }
+    `;
+  
+    const res = await graphqlQuery(query);
+    return res?.data?.transaction || [];
+  }
+    
